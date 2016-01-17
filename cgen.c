f4a57d85c4f01280e94c1af5c06bd59c0adf3322
Morten Krogh <morten.krogh@amberbio.com>
2016-01-17 17:06:46 +0100

    parse info

--- cgen.c
+++ cgen.c
@@ -6,14 +6,91 @@
 /*
  * The cgen program reads the configurstion file and the template file and produces a header and
  * source file for the specialized types and functions.  The program produces the two files and
- * exits or prints an error message to stderr and exits.  The program allocates memory and opens
- * files. Clean up is performed by the OS at program termination.  The entire cgen program is kept
- * in this source file for simplicity.
+ * exits or prints an error message to stderr and exits.  The program allocates memory. Freeing of
+ * memory is performed by the OS at program termination.  The entire cgen program is kept in this
+ * source file for simplicity.
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/* trim removes leading and trailing whitespace and returns the trimmed string. The argument is
+ * modified. str must have a null terminator */
+char *trim(char *str)
+{
+	char *start = str;
+	while (isspace(*start)) start++;
+
+	if (*start == '\0') return start;
+
+	char *end = start + strlen(start) - 1;
+	while (isspace(*end)) end--;
+
+	*(end + 1) = '\0';
+	
+	return start;
+}
+
+struct conf_info {
+	char *template_file;
+	char *heder_file;
+	char *source_file;
+	char **keys;
+	char **values;
+	int nkeys;
+};
+
+struct conf_info parse_conf_file(char *conf_file)
+{
+	struct conf_info conf_info = {NULL, NULL, NULL, NULL, NULL, 0};
+
+	FILE *file;
+	if ((file = fopen(conf_file, "r")) == NULL) {
+		fprintf(stderr, "The conf file %s could not be opened\n", conf_file);
+		exit(1);
+	}
+
+	char line[1024];
+	for (;;) {
+		if (fgets(line, sizeof line, file) == NULL) break;
+		if (strlen(line) >= sizeof line - 1) {
+			fprintf(stderr, "The conf file has too long lines\n");
+			exit(1);
+		}
+
+		char *right = line;
+		char *left = strsep(&right, "=");
+		if (right == NULL) continue;
+
+		left = trim(left);
+		right = trim(right);
+		
+		
+		printf("left = %s\n", left);
+		printf("right = %s\n", right);
+		
+		
+		
+		
+		printf("Length of line is %zu\n", strlen(line));
+	}
+
+	if (!feof(file)) {
+		fprintf(stderr, "There was a problem reading the conf file\n");
+		exit(1);
+	}
+
+	
 
 
+
+
+	fclose(file);
+	
+	return conf_info;
+}
 
 
 
@@ -31,6 +108,8 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	struct conf_info conf_info = parse_conf_file(argv[1]);
+	
 	
 
 
